156f46ae57bd3728920ceaffdf7b0d2ae3305a27
Matheus Machado <matheus_lm10@hotmail.com>
2023-07-13 19:45:28 -0400

    Initial commit from Create Next App

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,19 @@
+import { Story, Meta } from '@storybook/react'
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main,
+  args: {
+    title: 'title default',
+    description: 'description default'
+  }
+} as Meta
+
+export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: 'Basic Title',
+  description: 'Basic Description'
+}
+
+export const Default: Story = (args) => <Main {...args} />
