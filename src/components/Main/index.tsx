0736daab76d6820a1a21db9bde1d862d16707d17
Matheus Machado <matheus_lm10@hotmail.com>
2023-07-27 18:49:35 -0400

    Creates Logo component. - Shuts down TypesScript no-non-null-assertion.

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,20 +1,11 @@
 import * as S from './styles'
 
-const Main = ({
-  title = 'React Avançado',
-  description = 'TypeScript, ReactJS, NextJS e Styled Components'
-}) => (
+const Main = ({ title = 'React Avançado', description = 'TypeScript, ReactJS, NextJS e Styled Components' }) => (
   <S.Wrapper>
-    <S.Logo
-      src="/img/logo.svg"
-      alt="Imagem de um átomo - React Avançado escrito ao lado."
-    />
+    <S.Logo src="/img/logo.svg" alt="Imagem de um átomo - React Avançado escrito ao lado." />
     <S.Title>{title}</S.Title>
     <S.Description>{description}</S.Description>
-    <S.Ilustration
-      src="/img/hero-illustration.svg"
-      alt="Um desenvolvedor de frente para tela com código."
-    />
+    <S.Ilustration src="/img/hero-illustration.svg" alt="Um desenvolvedor de frente para tela com código." />
   </S.Wrapper>
 )
 
