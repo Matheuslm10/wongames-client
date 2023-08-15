aa82130e8887fbaece1599197f8cd3668de3cd34
Matheus Machado <matheus_lm10@hotmail.com>
2023-08-15 07:22:47 -0400

    Adds Ribbon to Banner component.

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -24,4 +24,30 @@ describe('<Banner />', () => {
     )
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('should render a Ribbon', () => {
+    renderWithTheme(
+      <Banner
+        img="images/my-image"
+        title="Defy death"
+        subtitle="Play the new CrashLands"
+        buttonLabel="Buy now"
+        buttonLink="/my-url"
+        ribbon="My Ribbon"
+        ribbonSize="small"
+        ribbonColor="secondary"
+      />
+    )
+
+    const ribbon = screen.getByText(/My Ribbon/i)
+
+    expect(ribbon).toBeInTheDocument()
+    expect(ribbon).toHaveStyle({
+      backgroundColor: '#3CD3C1'
+    })
+    expect(ribbon).toHaveStyle({
+      height: '2.6rem',
+      fontSize: '1.2rem'
+    })
+  })
 })
