7b270db764b11fcfe44b366f5739ffef2629a108
Matheus Machado <matheus_lm10@hotmail.com>
2023-08-22 20:06:09 -0400

    Fixes Container margin.

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -4,7 +4,7 @@ export const Container = styled.div`
   ${({ theme }) => css`
     max-width: ${theme.grid.container};
     margin-left: auto;
-    margin-right: auto
+    margin-right: auto;
     padding-left: calc(${theme.grid.gutter} / 2);
     padding-right: calc(${theme.grid.gutter} / 2);
   `}
--- src/components/Container/test.tsx
+++ src/components/Container/test.tsx
@@ -20,7 +20,8 @@ describe('<Container />', () => {
       .c0 {
         max-width: 130rem;
         margin-left: auto;
-        margin-right: auto padding-left:calc(3.2rem / 2);
+        margin-right: auto;
+        padding-left: calc(3.2rem / 2);
         padding-right: calc(3.2rem / 2);
       }
 
