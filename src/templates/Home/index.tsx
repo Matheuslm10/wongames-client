cde1265bcb6e4cd4785ab4b15a5ca94746abf472
Matheus Machado <matheus_lm10@hotmail.com>
2023-08-24 06:56:36 -0400

    Adds mock to Home page.

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,36 +1,77 @@
+import { BannerProps } from 'components/Banner'
+import { GameCardProps } from 'components/GameCard'
+import Highlight, { HighlightProps } from 'components/Highlight'
+
 import { Container } from 'components/Container'
-import Heading from 'components/Heading'
+import BannerSlider from 'components/BannerSlider'
 import Footer from 'components/Footer'
+import GameCardSlider from 'components/GameCardSlider'
+import Heading from 'components/Heading'
 import Menu from 'components/Menu'
 
-const Home = () => (
+export type HomeTemplateProps = {
+  banners: BannerProps[]
+  newGames: GameCardProps[]
+  mostPopularHighlight: HighlightProps
+  mostPopularGames: GameCardProps[]
+  upcommingHighlight: HighlightProps
+  upcommingGames: GameCardProps[]
+  upcommingMoreGames: GameCardProps[]
+  freeHighlight: HighlightProps
+  freeGames: GameCardProps[]
+}
+
+const Home = ({
+  banners,
+  newGames,
+  mostPopularHighlight,
+  mostPopularGames,
+  upcommingHighlight,
+  upcommingGames,
+  upcommingMoreGames,
+  freeHighlight,
+  freeGames
+}: HomeTemplateProps) => (
   <section>
     <Container>
       <Menu />
+      <BannerSlider items={banners} />
     </Container>
 
     <Container>
       <Heading lineLeft lineColor="secondary">
         News
       </Heading>
+
+      <GameCardSlider items={newGames} color="black" />
     </Container>
 
     <Container>
-      <Heading lineLeft lineColor="secondary" color="white">
+      <Heading lineLeft lineColor="secondary">
         Most Popular
       </Heading>
+
+      <Highlight {...mostPopularHighlight} />
+      <GameCardSlider items={mostPopularGames} />
     </Container>
 
     <Container>
-      <Heading lineLeft lineColor="secondary" color="white">
+      <Heading lineLeft lineColor="secondary">
         Upcomming
       </Heading>
+
+      <GameCardSlider items={upcommingGames} />
+      <Highlight {...upcommingHighlight} />
+      <GameCardSlider items={upcommingMoreGames} />
     </Container>
 
     <Container>
       <Heading lineLeft lineColor="secondary" color="white">
         Free Games
       </Heading>
+
+      <Highlight {...freeHighlight} />
+      <GameCardSlider items={freeGames} />
     </Container>
 
     <Container>
--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -1,11 +1,28 @@
+import 'match-media-mock'
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
+import bannersMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
 import Home from '.'
+
+const HomeProps = {
+  banners: bannersMock,
+  newGames: gamesMock,
+  mostPopularHighlight: highlightMock,
+  mostPopularGames: gamesMock,
+  upcommingHighlight: highlightMock,
+  upcommingGames: gamesMock,
+  upcommingMoreGames: gamesMock,
+  freeHighlight: highlightMock,
+  freeGames: gamesMock
+}
 
 describe('<Home />', () => {
   it('should render menu and footer', () => {
-    renderWithTheme(<Home />)
+    renderWithTheme(<Home {...HomeProps} />)
 
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
     expect(
@@ -14,7 +31,7 @@ describe('<Home />', () => {
   })
 
   it('should render the sections', () => {
-    renderWithTheme(<Home />)
+    renderWithTheme(<Home {...HomeProps} />)
 
     expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
     expect(
