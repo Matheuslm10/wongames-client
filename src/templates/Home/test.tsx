d8fe994115d9b0522962f5b30f65aa394ba7bbf3
Matheus Machado <matheus_lm10@hotmail.com>
2023-08-26 19:19:46 -0400

    Optimizes Home page mocks.

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -1,37 +1,37 @@
 import 'match-media-mock'
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
-
-import bannersMock from 'components/BannerSlider/mock'
+import bannerMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
-
 import Home from '.'
 
-const HomeProps = {
-  banners: bannersMock,
-  newGames: gamesMock,
+const props = {
+  banners: bannerMock,
+  newGames: [gamesMock[0]],
   mostPopularHighlight: highlightMock,
-  mostPopularGames: gamesMock,
+  mostPopularGames: [gamesMock[0]],
+  upcommingGames: [gamesMock[0]],
   upcommingHighlight: highlightMock,
-  upcommingGames: gamesMock,
-  upcommingMoreGames: gamesMock,
-  freeHighlight: highlightMock,
-  freeGames: gamesMock
+  upcommingMoreGames: [gamesMock[0]],
+  freeGames: [gamesMock[0]],
+  freeHighlight: highlightMock
 }
 
 describe('<Home />', () => {
   it('should render menu and footer', () => {
-    renderWithTheme(<Home {...HomeProps} />)
+    renderWithTheme(<Home {...props} />)
 
+    // menu
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
-    expect(
-      screen.getByRole('heading', { name: /contact us/i })
-    ).toBeInTheDocument()
-  })
 
-  it('should render the sections', () => {
-    renderWithTheme(<Home {...HomeProps} />)
+    // footer
+    expect(
+      screen.getByRole('heading', { name: /follow us/i })
+    ).toBeInTheDocument()
+
+    // logos (menu/footer)
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
 
     expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
     expect(
@@ -43,5 +43,12 @@ describe('<Home />', () => {
     expect(
       screen.getByRole('heading', { name: /free games/i })
     ).toBeInTheDocument()
+
+    // banner
+    expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
+    // card game ( 5 sections with 1 card each = 5x1 = 5)
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(5)
+    // highlight
+    expect(screen.getAllByText(/read dead is back!/i)).toHaveLength(3)
   })
 })
