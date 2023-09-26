f9c5af2a8ef1cce2ba00b5bb91f69553c06b6449
Matheus Machado <matheus_lm10@hotmail.com>
2023-09-26 19:35:46 -0400

    Creates Cart page.

---
+++ src/pages/cart.tsx
@@ -0,0 +1,22 @@
+import Cart, { CartTemplateProps } from 'templates/Cart'
+
+import itemsMock from 'components/CartList/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+import cardsMock from 'components/PaymentOptions/mock'
+
+export default function CartPage(props: CartTemplateProps) {
+  return <Cart {...props} />
+}
+
+export async function getServerSideProps() {
+  return {
+    props: {
+      items: itemsMock,
+      total: '$ 430,00',
+      cards: cardsMock,
+      recommendedGames: gamesMock.slice(0, 5),
+      recommendedHighlight: highlightMock
+    }
+  }
+}
