bae8bb5704e30d8f05b39452c9125a135cfe3abe
Matheus Machado <matheus_lm10@hotmail.com>
2023-11-02 16:34:20 -0400

    Creates add and remove cart behavior.

--- src/components/CartDropdown/index.tsx
+++ src/components/CartDropdown/index.tsx
@@ -1,19 +1,13 @@
 import CartIcon from 'components/CartIcon'
 import Dropdown from 'components/Dropdown'
-import { GameItemProps } from 'components/GameItem'
 
 import * as S from './styles'
 import CartList from 'components/CartList'
 
-export type CartDropdownProps = {
-  items?: GameItemProps[]
-  total?: string
-}
-
-const CartDropdown = ({ items, total }: CartDropdownProps) => (
+const CartDropdown = () => (
   <S.Wrapper>
     <Dropdown title={<CartIcon />}>
-      <CartList items={items} total={total} hasButton />
+      <CartList hasButton />
     </Dropdown>
   </S.Wrapper>
 )
--- src/components/CartDropdown/test.tsx
+++ src/components/CartDropdown/test.tsx
@@ -6,20 +6,23 @@ import { CartContextDefaultValues } from 'hooks/use-cart'
 import CartDropdown from '.'
 
 describe('<CartDropdown />', () => {
-  it('should render <CartIcon /> and its badge', () => {
-    render(<CartDropdown items={items} total="R$ 300,00" />, {
-      cartProviderProps: { ...CartContextDefaultValues, quantity: items.length }
-    })
+  beforeEach(() => {
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      items,
+      quantity: items.length,
+      total: 'R$ 300,00'
+    }
 
+    render(<CartDropdown />, { cartProviderProps })
+  })
+
+  it('should render <CartIcon /> and its badge', () => {
     expect(screen.getByLabelText(/shopping cart/i)).toBeInTheDocument()
     expect(screen.getByText(`${items.length}`)).toBeInTheDocument()
   })
 
   it('should render Dropdown content with cart items and total', () => {
-    render(<CartDropdown items={items} total="R$ 300,00" />, {
-      cartProviderProps: { ...CartContextDefaultValues, quantity: items.length }
-    })
-
     expect(screen.getByText('R$ 300,00')).toBeInTheDocument()
     expect(screen.getByText(`${items[0].title}`)).toBeInTheDocument()
   })
