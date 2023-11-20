90e8302ee8e2494130e9abf8526180969d7bfe16
Matheus Machado <matheus_lm10@hotmail.com>
2023-11-20 19:11:23 -0400

    Refactors deleting unused cards stuff.

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -5,8 +5,6 @@ import { gamesMapper, highlightMapper } from 'utils/mappers'
 
 import Cart, { CartTemplateProps } from 'templates/Cart'
 
-import itemsMock from 'components/CartList/mock'
-import cardsMock from 'components/PaymentOptions/mock'
 import protectedRoutes from 'utils/protected-routes'
 import { GetServerSidePropsContext } from 'next'
 
@@ -25,9 +23,6 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   return {
     props: {
       session,
-      items: itemsMock,
-      total: '$ 430,00',
-      cards: cardsMock,
       recommendedGamesTitle: data.recommended?.section?.title,
       recommendedGames: gamesMapper(data.recommended?.section?.games),
       recommendedHighlight: highlightMapper(
