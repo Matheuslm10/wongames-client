50d1d91466d8ec7b2a203fe5c63df853a7d078dc
Matheus Machado <matheus_lm10@hotmail.com>
2023-11-20 15:31:11 -0400

    Adds wishlistButton to GameInfo and fix tests.

--- src/components/GameCard/test.tsx
+++ src/components/GameCard/test.tsx
@@ -1,4 +1,5 @@
-import { render, fireEvent, screen } from 'utils/test-utils'
+import 'session.mock'
+import { render, screen } from 'utils/test-utils'
 
 import GameCard from '.'
 
@@ -61,21 +62,6 @@ describe('<GameCard />', () => {
     expect(screen.getByText('FREE')).toHaveStyle({
       'background-color': '#3CD3C1'
     })
-  })
-
-  it('should render a filled Favorite icon when favorite is true', () => {
-    render(<GameCard {...props} favorite />)
-
-    expect(screen.getByLabelText(/remove from wishlist/i)).toBeInTheDocument()
-  })
-
-  it('should call onFav method when favorite is clicked', () => {
-    const onFav = jest.fn()
-    render(<GameCard {...props} favorite onFav={onFav} />)
-
-    fireEvent.click(screen.getAllByRole('button')[0])
-
-    expect(onFav).toBeCalled()
   })
 
   it('should render Ribbon', () => {
--- src/components/GameInfo/index.tsx
+++ src/components/GameInfo/index.tsx
@@ -1,7 +1,5 @@
-import { FavoriteBorder } from '@styled-icons/material-outlined'
-
-import Button from 'components/Button'
 import CartButton from 'components/CartButton'
+import WishlistButton from 'components/WishlistButton'
 import Heading from 'components/Heading'
 import Ribbon from 'components/Ribbon'
 import formatPrice from 'utils/format-price'
@@ -20,12 +18,12 @@ const GameInfo = ({ id, title, description, price }: GameInfoProps) => (
     <Heading lineBottom>{title}</Heading>
 
     <Ribbon color="secondary">{formatPrice(price)}</Ribbon>
+
     <S.Description>{description}</S.Description>
+
     <S.ButtonsWrapper>
       <CartButton id={id} size="large" hasText />
-      <Button icon={<FavoriteBorder />} size="large" minimal>
-        Wishlist
-      </Button>
+      <WishlistButton id={id} hasText size="large" />
     </S.ButtonsWrapper>
   </S.Wrapper>
 )
