f8dc4149730faab856cd6eaf47f03da7e53f2b4a
Matheus Machado <matheus_lm10@hotmail.com>
2023-12-07 19:23:25 -0400

    Adds e2e tests to sign-in and sign-up pages.

---
+++ cypress/e2e/user.ts
@@ -0,0 +1,40 @@
+/// <reference path="../support/index.d.ts" />
+
+import { createUser } from '../support/generate'
+
+describe('User', () => {
+  it('should sign up', () => {
+    const user = createUser()
+
+    cy.visit('/sign-up')
+    cy.signUp(user)
+
+    cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+    cy.findByText(user.username).should('exist')
+  });
+
+  it('should sign in and sign out', () => {
+    cy.visit('/sign-in')
+    cy.signIn()
+
+    cy.findByText(/cypress/i).should('exist').click()
+    cy.findByText(/sign out/i).click()
+
+    cy.findByRole('link', { name: /sign in/i }).should('exist')
+    cy.findByText(/cypress/i).should('not.exist')
+  });
+
+  it('should sign the user and redirect to the page that it was previously defined', () => {
+    cy.visit('/profile/me')
+
+    // the user should be redirected to the login page if they try to access the protected page without a session
+    cy.location('href').should('eq', `${Cypress.config().baseUrl}/sign-in?callbackUrl=/profile/me`)
+
+    cy.signIn()
+
+    cy.location('href').should('eq', `${Cypress.config().baseUrl}/profile/me`)
+
+    cy.findByLabelText(/username/i).should('have.value', 'cypress')
+    cy.findByLabelText(/e-mail/i).should('have.value', 'e2e@wongames.com')
+  });
+});
